10e5586001b298979f47fc622522759cb2944a2c
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-12-02 17:30:53 +0200

    second

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { nanoid } from 'nanoid';
-// import ContactForm from 'components/ContactForm/ContactForm';
-// import ContactList from 'components/ContactList/ContactList';
-// model.id = nanoid() //=> "V1StGXR8_Z5jdHi6B-myT"
+import ContactForm from 'components/ContactForm/ContactForm';
+import Filter from 'components/Filter/Filter';
+import ContactList from 'components/ContactList/ContactList';
+import css from './App.module.css';
 
 class App extends Component {
   state = {
@@ -12,29 +12,30 @@ class App extends Component {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    name: '',
-    number: '',
     filter: '',
   };
 
-  handleChange = evt => {
-    const { name, value } = evt.target;
-    this.setState({ [name]: value });
-  };
+  addContact = newContact => {
+    const isInContacts = this.state.contacts.some(
+      ({ name }) => name.toLowerCase() === newContact.name.toLowerCase()
+    );
 
-  handleSubmit = evt => {
-    evt.preventDefault();
-    const { name, number } = this.state;
-    const newContact = {
-      id: nanoid(),
-      name,
-      number,
-    };
+    if (isInContacts) {
+      alert(`${newContact.name} is already in contacts`);
+      return;
+    }
+
     this.setState(prevState => ({
       contacts: [...prevState.contacts, newContact],
-      name: '',
-      number: '',
     }));
+  };
+
+  removeContact = contactId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+      };
+    });
   };
 
   onChangeFilter = evt => {
@@ -43,70 +44,30 @@ class App extends Component {
   };
 
   render() {
-    const { name, number, contacts, filter } = this.state;
-
+    const { contacts, filter } = this.state;
     // Фільтруємо контакти на основі значення поля пошуку
     const filteredContacts = contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
 
-    console.log(filteredContacts);
-
     return (
       <>
-        <div>
-          <h1>Phonebook</h1>
-        </div>
+        <div className={css.wrapper}>
+          <h1 className={css.title}>Phonebook</h1>
 
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleChange}
-              required
-            />
-          </label>
+          <ContactForm addContact={this.addContact} />
 
-          <label>
-            Number:
-            <input
-              type="tel"
-              name="number"
-              value={number}
-              onChange={this.handleChange}
-              required
-            />
-          </label>
-
-          <button type="submit">Add contact</button>
-        </form>
-
-        <div>
-          <h2>Find contacts by name</h2>
-          <input
+          <h2 className={css.title}>Contacts</h2>
+          <Filter
             name="filter"
             value={filter}
-            type="text"
-            onChange={this.onChangeFilter}
+            onChangeFilter={this.onChangeFilter}
+          />
+          <ContactList
+            contacts={filteredContacts}
+            removeContact={this.removeContact}
           />
         </div>
-
-        <div>
-          <h2>Contacts</h2>
-          <ul>
-            {filteredContacts.map(contact => (
-              <li key={contact.id}>
-                {contact.name} : {contact.number}
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        {/* Використовує фільтровані контакти для відображення */}
-        {/* <ContactList contacts={filteredContacts} /> */}
       </>
     );
   }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,39 @@
-// import css from './ContactList.module.css';
+import css from './ContactList.module.css';
+import PropTypes from 'prop-types';
 
-const ContactList = () => {
+const ContactList = ({ contacts, removeContact }) => {
   return (
     <>
-      <p></p>
+      <div>
+        <ul className={css.ul}>
+          {contacts.map(contact => (
+            <li key={contact.id} className={css.item}>
+              <p className={css.txt}>
+                {' '}
+                <span className={css.name}>{contact.name}: </span>{' '}
+                {contact.number}
+              </p>
+
+              {/* видалення */}
+              <button
+                className={css.btn}
+                type="button"
+                name="delete"
+                onClick={() => removeContact(contact.id)}
+              >
+                delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  removeContact: PropTypes.func.isRequired,
 };
 
 export default ContactList;
